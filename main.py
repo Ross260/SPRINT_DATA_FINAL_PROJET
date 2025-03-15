3d8783b986fe6eda2316e7292dc36595a6ac94fa
Ross260 <rossketcha@gmail.com>
2025-03-15 16:50:56 +0100

    recupération de toutes les information pour le projets et installation du connecteur pour la BD

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-
 url = "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
+urlc = "https://books.toscrape.com/catalogue/category/books/mystery_3/"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                          "Chrome/110.0.5481.104 Safari/537.36"}
 
@@ -16,30 +16,32 @@ if response.status_code == 200:
 
     # On crée un objet BeautifulSoup pour analyser le HTML
     soup = BeautifulSoup(response.text, "html.parser")
+
     # print(soup.prettify()) # affichage des informations de la page mais de manière formaté
+
     titles = soup.find_all('h3')
     prices = soup.find_all('p', class_='price_color')
     categories = soup.find_all('h1')
+    rating_elements = soup.find_all("p", class_="star-rating")
     liens = soup.find_all('span', class_='a-price-fraction')
-
 
     # Affichage du html de manière plus lisible
     print(soup.title.string)
 
     # Les noms de produits
-    #for title in titles:
-     #   print(title.string)
+    for title in titles:
+        print(title.string)
 
     # Les prix
-    """ for price in prices:
+    for price in prices:
         price_text = price.get_text(strip=True)  # Pour nettoyer les espaces inutiles
         clean_price = re.sub(r"[^\d.,]", "", price_text)  # supprésion de tous les caractères sauf chiffres, points et virgules
 
-        print(clean_price) """
+        print(clean_price)
 
     # Les catégories
     for category in categories:
-        print(category.string) # une seule catégorie par page
+        print(category.string)  # une seule catégorie par page
 
     # Dictionnaire pour convertir les nombres en texte vers des chiffres
     stars_mapping = {
@@ -51,8 +53,6 @@ if response.status_code == 200:
     }
 
     # Trouver l'élément qui contient la classe des étoiles
-    rating_elements = soup.find_all("p", class_="star-rating")
-
     for rating_element in rating_elements:
         if rating_element:
             # Extraire la classe exacte (ex: "star-rating Four")
@@ -75,7 +75,7 @@ if response.status_code == 200:
     for h3 in h3_tags:
         link = h3.find("a")  # Trouve un <a> à l'intérieur de <h3>
         if link and link.has_attr("href"):
-            print("Lien trouvé :", link["href"])  # Afficher seulement les liens dans <h3>
+            print("Lien trouvé :", urlc + link["href"])  # Afficher seulement les liens dans <h3>
         else:
             print("❌ Aucun lien dans ce <h3>.")
 else:
